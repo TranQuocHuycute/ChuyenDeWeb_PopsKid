aebf7b4d3788ada41ebd93273a9703a9a0960135
TranQuocHuy <20211tt1283@mail.tdc.edu.vn>
2022-10-22 00:28:53 +0700

    setup tailwind + redux

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import "./index.css";
+
 function App() {
   return (
-    <div className="App">
-      
-          Learn React
-       
+    <div className ="flex flex-row">
+      <button className="transition duration-150 ease-in-out  bg-red-600 mx-9 p-10 rounded-[10px]">Button A</button>
+      <button className="transition duration-300 ease-in-out  bg-red-600 mx-9 p-10 rounded-[10px]" >Button B</button>
+      <button className="transition duration-700 ease-in-out  bg-red-600 mx-9 p-10 rounded-[10px]">Button C</button>
     </div>
   );
 }
