410d4b62f69eaff899029db3d593375f25ddd904
Duy Khanh <dzerok2020@gmail.com>
2022-11-22 11:51:48 +0700

    init project

---
+++ Be/src/test/java/tv/dzerok1/popskids/PopsKidsApplicationTests.java
@@ -0,0 +1,13 @@
+package tv.dzerok1.popskids;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopsKidsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
