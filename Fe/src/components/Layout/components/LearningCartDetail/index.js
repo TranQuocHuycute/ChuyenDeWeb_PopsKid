2d814ce053577bfb3fff3de7aa06e0d76557163e
PhuTruongThanh <phutt.15.student@fit.tdc.edu.vn>
2022-12-23 02:10:16 +0700

    comit

--- Fe/src/components/Layout/components/LearningCartDetail/index.js
+++ Fe/src/components/Layout/components/LearningCartDetail/index.js
@@ -248,31 +248,33 @@ function LearningCartDetail() {
         </div>
       </div>
 
-      {/**/}
+      {/*lich hoc*/}
       <div className="flex justify-center align-center items-center sm:mt-10 mt-5">
         <span className="sm:text-[20px] text-[20px] xl:text-[24px] 2xl:text-[24px] font-bold leading-8 mr-2">
           Lịch học
         </span>
       </div>
-      {/* Lich hoc */}
       <div className="flex justify-center align-center items-center">
         <div className="flex flex-row justify-items-center items-center">
-          <a className="flex" href="...">
+          <span className="flex" href="...">
             <div className="mr-2 justify-center sm:flex-row flex-col items-center">
               <span className="sm:text-[20px] text-[12px] xl:text-[24px] 2xl:text-[24px] mr-2">
                 Ba Mẹ không tìm thấy thời gian phù hợp, liên hệ{''}
               </span>
-              <span className="sm:text-[20px] text-[12px] xl:text-[24px] 2xl:text-[24px] font-bold  text-[#06AFC3]">
+              <a
+                href="..."
+                className="sm:text-[20px] text-[12px] xl:text-[24px] 2xl:text-[24px] font-bold  text-[#06AFC3]"
+              >
                 090.181.6890{' '}
-              </span>
+              </a>
               <span className="sm:text-[20px] text-[12px] xl:text-[24px] 2xl:text-[24px]  mr-2">
                 để được tư vấn!
               </span>
             </div>
-          </a>
+          </span>
         </div>
       </div>
-      {/* learningcard */}
+      {/* Thời gian học */}
       <div className="flex justify-center align-center items-center pb-2">
         <div ref={dataLearningCard} className="flex flex-col sm:flex-row">
           {dataLearningCard.resources4.map((resource, index) => {
@@ -326,7 +328,7 @@ function LearningCartDetail() {
                         </div>
 
                         {/* tối đa học viên */}
-                        <div className='border-t border-[#fff] py-4'>
+                        <div className="border-t border-[#fff] py-4">
                           <a href={resource.link} className="">
                             <h3 className="text-[18px] font-bold text-red-500">
                               {resource.list}
@@ -356,6 +358,164 @@ function LearningCartDetail() {
               </div>
             )
           })}
+        </div>
+      </div>
+
+      {/* collapse_root__L5u__edittor */}
+      <div>
+        {/* Hoc vien se nhan dc gi */}
+        <div className="border-b border-[#f9f9f9] border-current pb:-[20px] sm:w-10/12 2xl:w-4/5 py-5 sm:m-auto">
+          <div className="flex justify-start items-center font-bold cusror-pointer  md:font-medium leading-7">
+            <p className="mr-4">Học Viên Sẽ Học Được Gì?</p>
+            <span className="cursor-pointer inline-block sm:mt-1 w-[10px] h-[10px] border-b-2 border-[#06afc3] border-r-2 rotate-45 md:rotate-[-135deg]"></span>
+          </div>
+          <div className="bg-[f9f9f9] text-[16px] leading-6 pt-[20px] none md:text-[20px] block">
+            <ul>
+              <li>
+                <span className="font-normal text-[14pt]">
+                  Học và thực hành các kỹ thuật biểu diễn cơ bản trong ảo thuật.
+                </span>
+              </li>
+              <li>
+                <span className="font-normal text-[14pt]">
+                  Tự tin cà chủ động xử lý huống trước đám đông
+                </span>
+              </li>
+              <li>
+                <span className="font-normal text-[14pt]">
+                  Hiểu và ghi nhớ các kiến thức khoa học - xã hội liên quan đến
+                  các màn trình diễn
+                </span>
+              </li>
+              <li>
+                <span className="font-normal text-[14pt]">
+                  Tăng cường sự linh hoạt trong vận động và óc quan sát
+                </span>
+              </li>
+              <p>
+                <span className="font-normal text-[14pt]">
+                  <span className="text-[14pt]">
+                    <strong>
+                      ☎ Hotline tư vấn khóa học và đăng ký học thử miễn phí:
+                    </strong>
+                  </span>
+                  <strong>
+                    <span className="text-[18pt]">
+                      <a className="text-[#4dbdc8]" href="tel:09018168">
+                        {' '}
+                        090.181.6890
+                      </a>
+                    </span>
+                  </strong>
+                </span>
+              </p>
+            </ul>
+          </div>
+        </div>
+        {/* Noi dung va giao trinh */}
+        <div className="border-b border-[#f9f9f9] border-current pb:-[20px] sm:w-10/12 2xl:w-4/5 py-5 sm:m-auto">
+          <div className="flex justify-start items-center font-bold cusror-pointer  md:font-medium leading-7">
+            <p className="mr-4">Nội dung và giáo trình</p>
+            <span className="cursor-pointer sm:mt-1 inline-block w-[10px] h-[10px] border-b-2 border-[#06afc3] border-r-2 rotate-45 md:rotate-[-135deg]"></span>
+          </div>
+          <div className="bg-[f9f9f9] text-[16px] leading-6 pt-[20px] none md:text-[20px] block">
+            <ul>
+              <li>
+                <span className="font-normal text-[14pt]">
+                  Tập nhảy TikTok Dance trên nền nhạc 2002
+                </span>
+              </li>{' '}
+            </ul>
+          </div>
+        </div>
+        {/* Tài liệu và công cụ học tập */}
+        <div className="border-b border-[#f9f9f9] border-current pb:-[20px] sm:w-10/12 2xl:w-4/5 py-5 sm:m-auto">
+          <div className="flex justify-start items-center font-bold cusror-pointer  md:font-medium leading-7">
+            <p className="mr-4">Tài liệu và công cụ học tập</p>
+            <span className="cursor-pointer inline-block sm:mt-1 w-[10px] h-[10px] border-b-2 border-[#06afc3] border-r-2 rotate-45 md:rotate-[-135deg]"></span>
+          </div>
+          <div className="bg-[f9f9f9] text-[16px] leading-6 pt-[20px] none md:text-[20px] block">
+            <ul>
+              <li>
+                <span className="font-normal text-[14pt]">
+                  Máy tính hoặc máy tính bảng có kết nối internet chất lượng tốt
+                </span>
+              </li>
+              <li>
+                <span className="font-normal text-[14pt]">
+                  Trang phục thể thao thoải mái
+                </span>
+              </li>
+              <li>
+                <span className="font-normal text-[14pt]">Giày bata</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+        {/* Lưu ý */}
+        <div className="border-b border-[#f9f9f9] border-current pb:-[20px] sm:w-10/12 2xl:w-4/5 py-5 sm:m-auto">
+          <div className="flex justify-start items-center font-bold cusror-pointer  md:font-medium leading-7">
+            <p className="mr-4">Lưu ý</p>
+            <span className="cursor-pointer inline-block sm:mt-1 w-[10px] h-[10px] border-b-2 border-[#06afc3] border-r-2 rotate-45 md:rotate-[-135deg]"></span>
+          </div>
+          <div className="bg-[f9f9f9] text-[16px] leading-6 pt-[20px] none md:text-[20px] block">
+            <ul>
+              <li>
+                <span className="font-normal text-[14pt]">
+                  Học và thực hành các kỹ thuật biểu diễn cơ bản trong ảo thuật.
+                </span>
+              </li>
+              <li>
+                <span className="font-normal text-[14pt]">
+                  Sắp xếp thời gian hợp lý để trẻ đảm bảo tham gia đủ thời lượng
+                  của 01 buổi học.
+                </span>
+              </li>
+              <li>
+                <span className="font-normal text-[14pt]">
+                  Quý Phụ huynh bố trí chỗ ngồi học thoáng, yên tĩnh và đủ ánh
+                  sáng cho trẻ.
+                </span>
+              </li>
+              <li>
+                <span className="font-normal text-[14pt]">
+                  Với các trẻ dưới 6 tuổi, quý phụ huynh hỗ trợ đăng nhập trước
+                  giờ học.
+                </span>
+              </li>
+
+              <li>
+                <span className="font-normal text-[14pt]">
+                  Lớp học điệu nhảy hot trên Tiktok sẽ được diễn ra khi đủ sĩ số
+                  tối thiểu.
+                </span>
+              </li>
+            </ul>
+          </div>
+        </div>
+        {/* Thông tin về giáo viên */}
+        <div className="border-b border-[#f9f9f9] border-current pb:-[20px] sm:w-10/12 2xl:w-4/5 py-5 sm:m-auto">
+          <div className="flex justify-start items-center font-bold cusror-pointer  md:font-medium leading-7">
+            <p className="mr-4">Thông tin về giáo viên/Học viện</p>
+            <span className="cursor-pointer inline-block sm:mt-1 w-[10px] h-[10px] border-b-2 border-[#06afc3] border-r-2 rotate-45 md:rotate-[-135deg]"></span>
+          </div>
+          <div className="bg-[f9f9f9] text-[16px] leading-6 pt-[20px] none md:text-[20px] block">
+            <img
+              className="block ml-auto mr-auto"
+              src="https://vnw-img-cdn.popsww.com/api/v2/containers/file2/cms_thumbnails/co_trang_kk-8d701e2d4592-1653896198872-lFiFhXZf.png?v=0"
+              alt width='200' height='243'
+            />
+
+            <ul>
+              <li>
+                <span className="font-normal text-[14pt]">
+                  Cô Trang KK là dancer với 13 năm kinh nghiệm. Hiện nay cô đang
+                  là giáo viên dạy Dance, K-Pop, US-UK, Zumba FITNESS và biên
+                  đạo cho các vũ đoàn tại Thành phố Hồ Chí Minh.
+                </span>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
